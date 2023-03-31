37b3bde337af8a1a5b52092b6a99a56f44d009ab
Ganchenko-Ilya <chicha3d@gmail.com>
2023-03-31 18:55:21 +0300

    Completed task 6 kyu

---
+++ Js1/js6.js
@@ -0,0 +1,32 @@
+// You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+
+// Examples
+// []                   -->  0
+// [1, 2, 3]            -->  3
+// ["x", "y", ["z"]]    -->  4
+// [1, 2, [3, 4, [5]]]  -->  7
+// The input will always be an array.
+function deepCount(a) {
+  let countResult = 0;
+
+  a.map((el) => {
+    const count = (x) => {
+      if (typeof x === "object") {
+        countResult++;
+        x.map((item) => {
+          if (typeof item === "object") {
+            count(item);
+          } else {
+            countResult++;
+          }
+        });
+      } else {
+        countResult++;
+      }
+    };
+
+    count(el);
+  });
+  return countResult;
+}
+console.log(deepCount([1, 2, 3, [4, 5, [6, 7]]]));
